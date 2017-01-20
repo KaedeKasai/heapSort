c44cc47f0e4b1b5c3931441e2aedbe0eb247e94e
KaedeKasai <x16u009@chiba-fjb.ac.jp>
2017-01-20 11:14:49 +0900

    完成
    やりました

--- ToStringK.java
+++ ToStringK.java
@@ -2,39 +2,44 @@ package heapSort;
 
 public class ToStringK {
 	
-	int size = 8;
-	int[] arr = {-1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63};
+	int size = 126;
+	int[] heap = {-1,42,29,18,14,7,18,12,11,13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	
 	public void toStringK(){
 		
-		;
+		
 		int height = 0;
 		
 		for(int i = 1;size >= i;i *= 2){
 			height++;
 		}
 		
-		
-		int space = 6;
-		int sideSpace = 3;
+		int space           = 6;
+		int sideSpace       = 3;
+		int slashSpace      = 7;
+		int sideSlashSpace  = 7;
+		int slashWidthSpace = 3;
 		
 		sideSpace = (sideSpace * 2) + 2;
+		slashSpace      = (slashSpace * 2);
 		
 		for(int i = 3;height > i;i++){
 			
-			space     = (space * 2) + 4;
-			sideSpace = (sideSpace * 2) + 2;
+			space           = (space * 2) + 4;
+			sideSpace       = (sideSpace * 2) + 2;
+			slashSpace      = (slashSpace * 2) + 1;
+			sideSlashSpace  = sideSlashSpace * 2;
+			slashWidthSpace = (slashWidthSpace * 2) + 2;
 			
 		}
-		System.out.println(space);
-		System.out.println(sideSpace);
+		if(height == 2){
+			makeSpace(3);
+		} else {
+			makeSpace(sideSpace);
+		}
 		
+		makeChild(1);
 		
-		makeSpace(sideSpace);
-		
-		makeNode(1);
-		
-		System.out.println();
 		System.out.println();
 		
 		int heightNode = 2;
@@ -44,33 +49,55 @@ public class ToStringK {
 		
 		for(int i = 1;height - 1 > i;i++){
 			
-			makeSpace(sideSpace);
-			
-			for(int j = 0;heightNode > j;j++){
-			
-			makeNode(countNode);
-			countNode++;
-			
-			makeSpace(space);
-			
+			if(height == 3){
+				makeSpace(6);
+			} else {
+				makeSpace(sideSlashSpace + 1);
+				sideSlashSpace = (sideSlashSpace - 1)/ 2;
 			}
 			
-			heightNode *= 2;
-			space = (space - 4) / 2;
-			sideSpace = (sideSpace - 2) / 2;
+			System.out.print("／");
+			
+			for(int j = 0;heightNode - 1 > j;j++){
+				
+				
+				if(j % 2 == 0){
+					
+					makeSpace(slashWidthSpace);
+					System.out.print("＼");
+					
+				} else {
+					
+					makeSpace(slashSpace);
+					System.out.print("／");
+					
+				}
+				
+			}
+			slashWidthSpace = (slashWidthSpace - 2) / 2;
+			slashSpace = (slashSpace - 2) / 2;
 			
 			System.out.println();
-//			while(size >= countNode){
-//				
-//				System.out.print("  ／");
-//				countNode++;
-//				if(size < countNode){
-//					break;
-//				}
-//				System.out.print(" ＼   ");
-//				
-//				
-//			}
+			
+			
+			makeSpace(sideSpace);
+		
+			for(int j = 0;heightNode > j;j++){
+				
+				makeChild(countNode);
+				countNode++;
+				
+				
+				if(heightNode - 1 != j){
+					makeSpace(space);
+				}
+			}
+			
+			heightNode = heightNode * 2;
+			space      = (space - 4) / 2;
+			sideSpace  = (sideSpace - 2) / 2;
+			
+			System.out.println();
 			
 		}
 		
@@ -78,14 +105,12 @@ public class ToStringK {
 		
 		while(size >= countNode){
 			
-			System.out.print("  ／");
-			countNode++;
-			if(size < countNode){
-				break;
+			if(countNode % 2 == 0){
+				System.out.print("  ／");	
+			} else {
+				System.out.print(" ＼   ");
 			}
-			System.out.print(" ＼   ");
-			
-			
+			countNode++;
 		}
 		System.out.println();
 		
@@ -93,7 +118,7 @@ public class ToStringK {
 		
 		while(size >= countNode){
 			
-			makeNode(countNode);
+			makeChild(countNode);
 			countNode++;
 			
 			System.out.print(" ");
@@ -107,9 +132,7 @@ public class ToStringK {
 		}
 	}
 	
-	public void makeNode(int index){
-		
-		System.out.printf("[" + "%2d" + "]",arr[index]);
-		
+	public void makeChild(int index){
+		System.out.printf("[" + "%2d" + "]",heap[index]);
 	}
 }
